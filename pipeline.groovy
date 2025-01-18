97c1bfb36e1828cc73fc02774ed8c96f548ff6a4
irushiuk <133183572+irushiuk@users.noreply.github.com>
2025-01-18 04:55:01 -0500

    writing the pipeline to run the helloworld file

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,1 +1,19 @@
-
+pipeline {
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                // Checkout the repository code
+                checkout scm
+            }
+        }
+        stage('Run Python Script') {
+            steps {
+                // Run the Helloworld.py script
+                script {
+                    sh "python3 hello_world.py"
+                }
+            }
+        }
+    }
+}
